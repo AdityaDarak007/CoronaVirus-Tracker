38a1e1f107274a40065779728a913dfcf82655e4
Aditya Maheshwari <73491810+AdityaDarak007@users.noreply.github.com>
2022-08-22 18:44:47 +0530

    Add files via upload

---
+++ CoronaVirus/coronavirus/src/main/java/com/io/javabrains/coronavirus/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.io.javabrains.coronavirus.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.io.javabrains.coronavirus.models.LocationStats;
+import com.io.javabrains.coronavirus.service.coronaVirusDataService;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    coronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+
+}
